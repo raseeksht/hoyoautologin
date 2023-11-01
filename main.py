dd74e861bc07c1183bb437fa359084db70c7b059
raseeksht <raseeksht1@gmail.com>
2023-11-01 09:50:08 +0545

    auto create folder and copy .env file to c:/Users/<username>/AppData/Local/hoyoAutoLogin/

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import sys
 import shutil
 
-load_dotenv()
+
 
 headers = {
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
@@ -25,7 +25,6 @@ def baseDaily(reqUrl,payload,headers):
         sys.exit()
 
     response = requests.post(reqUrl, data=json.dumps(payload),  headers=headers)
-
     return response
     
 
@@ -34,10 +33,10 @@ def honkaiDaily():
     print("Daily Signin Honkai StarRail:")
 
     reqUrl = "https://sg-public-api.hoyolab.com/event/luna/os/sign"
-    cookie = os.getenv("Cookie") if os.getenv("SameAccount") else os.getenv("HonkaiCookie")
+    cookie = getenv("Cookie") if getenv("SameAccount") else getenv("HonkaiCookie")
 
     payload = {
-        "act_id": os.getenv("HonkaiActId"),
+        "act_id": getenv("HonkaiActId"),
         "lang": "en-us"
     }
 
@@ -53,17 +52,16 @@ def honkaiDaily():
 def genshinDaily():
     print("Daily Signin Genshin Impact:")
     reqUrl = "https://sg-hk4e-api.hoyolab.com/event/sol/sign?lang=en-us"
-    cookie = os.getenv("Cookie") if os.getenv("SameAccount") else os.getenv("GenshinCookie")
+    cookie = getenv("Cookie") if getenv("SameAccount") else getenv("GenshinCookie")
     payload = {
         "act_id": os.getenv("GenshinActId"),
         "lang": "en-us"
     }
-    # print(payload)
-
     headers["Cookie"] = cookie
-
     response = baseDaily(reqUrl,payload, headers)
-
+    # if response.json()['message'].lower() == 'ok':
+    #     with open(dateFile,"w") as f:
+    #         f.write(dateToday)
     print(response.json()['message'],end="\n\n")
 
 def checkAlreadyLogged(dateFile):
@@ -72,8 +70,15 @@ def checkAlreadyLogged(dateFile):
             prevdate = f.read()
             if prevdate == dateToday:
                 print("Already Logged In")
+                showExitDialogue()
                 sys.exit()
     return 1
+
+
+def showExitDialogue():
+    if getenv("ExitDialogue") == '1':
+        input("Enter to exit...")
+
 
 # def checkEnvFile(logDir):
 #     envFile = os.path.join(logDir,".env")
@@ -83,26 +88,34 @@ def checkAlreadyLogged(dateFile):
 #         print("envfile already exists")
 
 
+def getenv(var):
+    return os.getenv(var)
 
 
 if __name__ == '__main__':
-    username = os.getenv("USERNAME")
+    username = getenv("USERNAME")
     logDir = fr"C:\Users\{username}\AppData\Local\hoyoAutoLogin"
 
     if not os.path.exists(logDir):
+        print(f"creating {logDir}")
         os.mkdir(logDir)
+        shutil.copy(".env",os.path.join(logDir,".env"))
+    
+    load_dotenv(dotenv_path=os.path.join(logDir,".env"))
+    
     dateFile = os.path.join(logDir,"date.txt")
     dateToday = datetime.now().strftime("%D")
 
     checkAlreadyLogged(dateFile)
 
     try:
-        honkaiDaily()
-        genshinDaily()
+        honkaiDaily() if getenv("HonkaiLogin") == '1' else ''
+        genshinDaily() if getenv("GenshinLogin") == '1' else ''
         dateToday = datetime.now().strftime("%D")
-        with open(dateFile,"w") as f:
-            f.write(dateToday)
+        if getenv("HonkaiLogin")=='1' or getenv("GenshinLogin")=='1':
+            with open(dateFile,"w") as f:
+                f.write(dateToday)
     except Exception as e:
         print(f"Error Occured: {e}")
     
-    # input("Enter to exit...")
+    showExitDialogue()
